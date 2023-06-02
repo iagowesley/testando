833e76d70500a3024cc3dafacc585c54e277e411
Iago Wesley Irineu Costa <iago.costa@fitbank.com.br>
2023-06-02 14:10:42 -0300

    tesdte

---
+++ index.js
@@ -0,0 +1,6 @@
+const menuIcon = document.querySelector('.menu-icon');
+const menuDropdown = document.querySelector('.menu-dropdown');
+
+menuIcon.addEventListener('click', () => {
+    menuDropdown.classList.toggle('active');
+});
